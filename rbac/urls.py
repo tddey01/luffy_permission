ed2e4e3b40fc15032f7266fcc3c4bdcc3aa1214f
kevinlandun@gmail.com <1qaz2wsx3edcQA1@>
2019-09-09 17:11:48 +0800

    RoleModelForm

--- rbac/urls.py
+++ rbac/urls.py
@@ -6,7 +6,7 @@ app_name = 'rbac'
 urlpatterns = [
     url(r'^role/list/$',role.role_list,name='role_list'),  # rbac:role_list
     url(r'^role/add/$', role.role_add,name='role_add'), # rbac:role_add
-    url(r'^role/del/$', role.role_del, name='role_del'),  # rbac:role_del
+    url(r'^role/del/(?P<pk>\d+)/$', role.role_del, name='role_del'),  # rbac:role_del
     url(r'^role/edit/(?P<pk>\d+)/$', role.role_edit, name='role_edit'),  # rbac:role_edit
 
 
